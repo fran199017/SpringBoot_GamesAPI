91548e48badd37031a62393bde2c302a2f05a23b
Francisco Nicolau <fran199017@gmail.com>
2022-12-02 21:00:29 +0100

    update to heroku

--- src/main/java/com/example/gamesapi/gamesapi/users/model/Users.java
+++ src/main/java/com/example/gamesapi/gamesapi/users/model/Users.java
@@ -1,23 +1,31 @@
 package com.example.gamesapi.gamesapi.users.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@JsonSerialize()
 public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "ID", required = true)
     @Column(name = "ID", nullable = false)
     private int id;
 
     @Column(name = "EMAIL", nullable = false,  unique = true, length = 45)
+    @ApiModelProperty(value = "EMAIL", required = true)
     private String email;
 
     @Column(name = "PASSWORD",nullable = false, length = 64)
+    @ApiModelProperty(value = "PASSWORD", required = true)
     private String password;
 
     @Column(name = "ROLE", nullable = false)
+    @ApiModelProperty(value = "ROLE", required = true)
     private String role;
 
     public String getRole() {
